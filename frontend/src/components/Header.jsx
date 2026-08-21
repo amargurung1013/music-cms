47a02ac78aa42e47238ca9d3bd064f58e7dbfbda
amar <amargurung1013@gmail.com>
2026-08-21 23:25:40 +0530

    Add contextual back link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Header() {
+  const location = useLocation()
+  const isDetailPage = location.pathname.startsWith('/albums/')
+
   return (
     <header className="site-header">
-      <Link to="/" className="wordmark">ARCHIVE</Link>
+      {isDetailPage && <Link to="/discography" className="wordmark">Back</Link>}
       <nav className="site-nav" aria-label="Main navigation">
         <Link to="/discography">Discography</Link>
         <Link to="/search">Search</Link>
